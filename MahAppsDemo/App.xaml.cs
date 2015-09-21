d68c4cbd4a727c5fe5c0471528d3a79b8a622516
Lin Yu <llinyu@gmail.com>
2015-09-21 21:47:53 +0800

    DynamicResource demo on multithread UI

---
+++ MahAppsDemo/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using MahApps.Metro;
+
+namespace MahAppsDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            var theme = ThemeManager.DetectAppStyle(this);
+            var appTheme = ThemeManager.GetAppTheme("BaseDark");
+            ThemeManager.ChangeAppStyle(this, theme.Item2, appTheme);
+
+            Splash.Instance.Show();
+            var win = new MainWindow();
+            win.Show();
+            Splash.Instance.Close();
+        }
+    }
+}
---
+++ MahAppsDemo/Splash.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace MahAppsDemo
+{
+    /// <summary>
+    /// Splash screen control
+    /// </summary>
+    public sealed class Splash
+    {
+        private SplashWindow _splash;
+        private Dispatcher _dispatcher;
+        private bool _isShowing;
+
+        public static Splash Instance { get; } = new Splash();
+
+        /// <summary>
+        /// Show splash screen.
+        /// </summary>
+        public void Show()
+        {
+            if (_isShowing)
+            {
+                return;
+            }
+
+            var signal = new AutoResetEvent(false);
+
+            ThreadStart showSplash = () =>
+            {
+                _splash = new SplashWindow();
+
+                _dispatcher = _splash.Dispatcher;
+
+                _dispatcher.BeginInvoke((Action)(() =>
+                {
+                    _splash.Closed += (s, e) =>
+                    {
+                        _isShowing = false;
+                        _dispatcher.InvokeShutdown();
+                    };
+                    _splash.Show();
+                    _isShowing = true;
+                    signal.Set();
+                }));
+
+                Dispatcher.Run();
+            };
+
+            var splashThread = new Thread(showSplash)
+            {
+                Name = "SplashThread",
+                IsBackground = true
+            };
+            splashThread.SetApartmentState(ApartmentState.STA);
+            splashThread.Start();
+
+            signal.WaitOne();
+        }
+        
+        /// <summary>
+        /// Close splash screen
+        /// </summary>
+        public void Close()
+        {
+            if (_isShowing)
+            {
+                _dispatcher.Invoke(() => _splash.Close());
+            }
+        }
+
+    }
+}
